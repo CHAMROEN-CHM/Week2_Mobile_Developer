d72fcb8de3540dd05121f2a37d3026dd5cea1acc
Chhun_Chamroen <chamroenstd@gmail.com>
2026-05-18 14:17:02 +0700

    Update W2_S2.dart

--- W2_S2.dart
+++ W2_S2.dart
@@ -153,62 +153,6 @@ void main() {
 }
 
 Exercise 3 : Duration
-class MyDuration {
-  // Display the duration in a readable format
-  final int _milliseconds;
-  // default constructor
-  MyDuration(this._milliseconds) {
-    if (_milliseconds < 0) {
-      throw Exception("Can't be Negative.");
-    }
-  }
-  // named constructors
-  MyDuration.fromHours(int h) : _milliseconds = h * 3600000;
-  MyDuration.fromMinutes(int min) : _milliseconds = min * 60000;
-  int get milliseconds => _milliseconds;
-
-  // + operator
-  MyDuration operator +(MyDuration other) {
-    return MyDuration(_milliseconds + other._milliseconds);
-  }
-
-  // - operator
-  MyDuration operator -(MyDuration other) {
-    return MyDuration(_milliseconds - other._milliseconds);
-  }
-
-  // compare operator
-  bool operator <(MyDuration other) {
-    return _milliseconds < other._milliseconds;
-  }
-
-  bool operator >(MyDuration other) {
-    return _milliseconds > other._milliseconds;
-  }
-
-  @override
-  String toString() {
-    int seconds = (_milliseconds / 1000).round();
-    int minutes = (seconds / 60).floor();
-    seconds = seconds % 60;
-    int hours = (minutes / 60).floor();
-    minutes = minutes % 60;
-    return '$hours hours, $minutes minutes, $seconds seconds';
-  }
-}
-
-void main() {
-  MyDuration duration1 = MyDuration.fromHours(3); // 3 hours
-  MyDuration duration2 = MyDuration.fromMinutes(45); // 45 minutes
-  print(duration1 + duration2); // 3 hours, 45 minutes, 0 seconds
-  print(duration1 > duration2); //true
-
-  try {
-    print(duration2 - duration1); // This will throw an exception
-  } catch (e) {
-    print(e);
-  }
-}
 
 Exercise 4 : Bonus : Shop Management
 class address
